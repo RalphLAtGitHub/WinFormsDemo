00f95f3e1bbebcf98dbf4abc61961fcc0f076a91
RalphLAtGitHub <mail@ralphlechterbeck.de>
2022-12-28 11:53:56 +0100

    Add files via upload

---
+++ WinFormsDemo/WinFormsDemo/ApplicationEvents.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+
+Namespace My
+
+    Partial Friend Class MyApplication
+        Private Sub MyApplication_StartupNextInstance(sender As Object, e As StartupNextInstanceEventArgs) Handles Me.StartupNextInstance
+
+            Debug.WriteLine("StartupNextInstance event raised.")
+
+            Dim m_TestForm As New Form With {.MdiParent = Form1, .Text = "TestForm 2"}
+            m_TestForm.Show()
+            m_TestForm.Location = New Point(50, 50)
+
+            Debug.WriteLine("StartupNextInstance event finished.")
+
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ WinFormsDemo/WinFormsDemo/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Protected Overrides Sub OnLoad(e As EventArgs)
+
+        MyBase.OnLoad(e)
+        Dim m_TestForm As New Form With {.MdiParent = Me, .Text = "TestForm 1"}
+        m_TestForm.Show()
+        m_TestForm.Location = New Point(10, 10)
+
+    End Sub
+
+End Class
