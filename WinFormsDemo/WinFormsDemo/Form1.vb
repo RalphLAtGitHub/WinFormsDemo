ae4d554852095934627717edc0c09299c38e248d
RalphLAtGitHub <mail@ralphlechterbeck.de>
2022-12-30 07:40:48 +0100

    Update Form1.vb

--- WinFormsDemo/WinFormsDemo/Form1.vb
+++ WinFormsDemo/WinFormsDemo/Form1.vb
@@ -1,12 +1,27 @@
-﻿Public Class Form1
+Imports System.IO
+Imports System.Reflection
+
+Public Class Form1
 
     Protected Overrides Sub OnLoad(e As EventArgs)
 
         MyBase.OnLoad(e)
+
+        Dim m_Button As New Button With {.Font = New Font(Me.Font, FontStyle.Bold), .Height = 50, .Width = Me.ClientRectangle.Width - 6,
+            .Text = "Start 2nd instance"}
+        Me.Controls.Add(m_Button)
+        m_Button.Location = New Point(Me.ClientRectangle.Right - m_Button.Width - 3, Me.ClientRectangle.Bottom - m_Button.Height - 3)
+        AddHandler m_Button.Click, AddressOf ButtonClickHandler
+
         Dim m_TestForm As New Form With {.MdiParent = Me, .Text = "TestForm 1"}
         m_TestForm.Show()
         m_TestForm.Location = New Point(10, 10)
+        m_Button.Select()
 
     End Sub
 
-End Class
+    Private Sub ButtonClickHandler(sender As Object, e As EventArgs)
+
+        Process.Start(Application.ExecutablePath)
+
+    End Sub
